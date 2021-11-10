c7ce8641764e29684c5ad7feb16b3132cbf98673
maulanas13 <maulana.septanto@hotmail.com>
2021-11-11 02:41:17 +0700

    TDL-5, developed ability to detect form input value, next to develop how it can send data to BE

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -3,11 +3,41 @@ import "./styles/RegisterPage.css"
 
 function RegisterPage() {
     const [showPass, setShowPass] = useState("password");
+    const [checkPass, setCheckPass] = useState("");
+    const [registerInput, setRegisterInput] = useState({
+        email: "",
+        username: "",
+        password: "",
+    });
 
     const inputEmailRef = useRef();
     const inputUsernameRef = useRef();
     const inputPassRef = useRef();
     const inputConfirmPassRef = useRef();
+    const regisBtnRef = useRef();
+
+    // HANDLER FUNCTIONS SECTION
+    const regisMailChangeHandler = (event) => {
+        setRegisterInput((prevState) => {
+        return { ...prevState, email: event.target.value.toLowerCase() };
+        });
+    };
+
+    const regisUserChangeHandler = (event) => {
+        setRegisterInput((prevState) => {
+        return { ...prevState, username: event.target.value };
+        });
+    };
+
+    const regisPassChangeHandler = (event) => {
+        setRegisterInput((prevState) => {
+        return { ...prevState, password: event.target.value };
+        });
+    };
+
+    const confirmPassChangeHandler = (event) => {
+        setCheckPass(event.target.value);
+    };
 
     const showPassHandler = (event) => {
         if (event.target.checked) {
@@ -24,6 +54,11 @@ function RegisterPage() {
         }
     }
 
+    // CLICK FUNCTION SECTION
+    const onSubmitRegister = async (event) => {
+        event.preventDefault();
+    }
+
     return (
         <div className="register-main-wrap">
             <div className="register-input-wrap">
@@ -34,8 +69,8 @@ function RegisterPage() {
                         type="email"
                         className="form-control shadow-none"
                         ref={inputEmailRef}
-                        // value={registerInput.email}
-                        // onChange={regisMailChangeHandler}
+                        value={registerInput.email}
+                        onChange={regisMailChangeHandler}
                         onKeyUp={(event) => onEnterSwitch(event, inputUsernameRef)}
                     />
                     <p>Create Username <span className="text-danger">(case sensitive) *</span></p>
@@ -43,8 +78,8 @@ function RegisterPage() {
                         type="text"
                         className="form-control shadow-none"
                         ref={inputUsernameRef}
-                        // value={registerInput.username}
-                        // onChange={regisUserChangeHandler}
+                        value={registerInput.username}
+                        onChange={regisUserChangeHandler}
                         onKeyUp={(event) => onEnterSwitch(event, inputPassRef)}
                     />
                     <p>Create Password <span className="text-danger">(case sensitive) *</span></p>
@@ -52,8 +87,8 @@ function RegisterPage() {
                         type={showPass}
                         className="form-control shadow-none"
                         ref={inputPassRef}
-                        // value={registerInput.password}
-                        // onChange={regisPassChangeHandler}
+                        value={registerInput.password}
+                        onChange={regisPassChangeHandler}
                         onKeyUp={(event) => onEnterSwitch(event, inputConfirmPassRef)}
                     />
                     <p>Confirm Password <span className="text-danger">(must match) *</span></p>
@@ -61,7 +96,8 @@ function RegisterPage() {
                         type={showPass}
                         className="form-control shadow-none"
                         ref={inputConfirmPassRef}
-                        // onChange={confirmPassChangeHandler}
+                        onChange={confirmPassChangeHandler}
+                        onKeyUp={(event) => onEnterSwitch(event, regisBtnRef)}
                     />
                     <div className="mb-4">
                         <input
@@ -75,15 +111,12 @@ function RegisterPage() {
                 <div className="register-btn-wrap">
                     <button 
                         form="registerForm"
+                        type="submit"
                         className="btn btn-success shadow-none"
+                        ref={regisBtnRef}
+                        onClick={onSubmitRegister}
                     >
                         Submit
-                    </button>
-                    <button 
-                        form="registerForm"
-                        className="btn btn-danger shadow-none"
-                    >
-                        Cancel
                     </button>
                 </div>
             </div>
