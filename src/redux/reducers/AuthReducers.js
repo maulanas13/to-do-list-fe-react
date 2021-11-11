81b4cba44c3261b479ecbf92c00bfae515fdd7d8
gangsararyop <86463063+gangsararyop@users.noreply.github.com>
2021-11-11 15:39:55 +0700

    tambah file dan folder

---
+++ src/redux/reducers/AuthReducers.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+  id: null,
+  username: "",
+  email: "",
+  role_id: null,
+  is_login: null,
+  is_verified: null,
+};
+
+const AuthReducers = (state = INITIAL_STATE, action) => {
+  switch (type) {
+    case value:
+      return;
+
+    default:
+      return state;
+  }
+};
